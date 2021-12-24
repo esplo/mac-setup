e0db815e3dd5d0e6ac7ad6df209ea0813f75b176
esplo <esplo@users.noreply.github.com>
2021-12-24 19:07:47 +0900

    modernized

--- setup.sh
+++ setup.sh
@@ -2,14 +2,6 @@
 
 set -eux
 
-function open_dmg {
-  mkdir -p /tmp/$1
-  pushd /tmp/$1
-  curl -L -o ./t.dmg $2
-  open -W ./t.dmg
-  popd
-}
-
 ## Show Hidden Files
 defaults write com.apple.finder AppleShowAllFiles TRUE
 
@@ -17,37 +9,38 @@ defaults write com.apple.finder AppleShowAllFiles TRUE
 defaults write -g KeyRepeat -int 2
 defaults write -g InitialKeyRepeat -int 15
 
+## install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-cd /tmp
-## Google Chrome
-open_dmg gc https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
 
-## K
-open_dmg ke https://pqrs.org/osx/karabiner/files/Karabiner-Elements-12.1.0.dmg
+## install apps via brew
 
-## Brew (needs hitting Enter)
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install git
+brew install --cask google-chrome
+brew install --cask karabiner-elements
+brew install --cask iterm2
+brew install --cask clipy
+brew install --cask wavebox
+brew install --cask dropbox
+brew install --cask visual-studio-code
+
 
 ## ZSH (needs password)
 
 brew install zsh
 sudo sh -c "echo $(which zsh) >> /etc/shells"
 chsh -s $(which zsh)
-
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
 cat << 'EOF' > /tmp/prezto-cmd.sh
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 EOF
-
 $(which zsh) /tmp/prezto-cmd.sh
 
 brew install autojump
 brew install peco
-
 
 cat << 'EOF' >> ~/.zshrc
 setopt auto_cd
@@ -63,27 +56,3 @@ function peco-history-selection() {
 zle -N peco-history-selection
 bindkey '^R' peco-history-selection
 EOF
-
-## iTerm 2
-brew cask install iterm2
-
-## Clipy
-open_dmg clipy https://github.com/Clipy/Clipy/releases/download/1.2.1/Clipy_1.2.1.dmg
-
-## Sequel Pro
-open_dmg seq https://github.com/sequelpro/sequelpro/releases/download/release-1.1.2/sequel-pro-1.1.2.dmg
-
-## Wavebox
-open_dmg wb https://github.com/wavebox/waveboxapp/releases/download/v4.4.0/Wavebox_4_4_0_osx.dmg
-
-## Google Cloud SDK (needs hitting Enter and Y)
-curl https://sdk.cloud.google.com | bash
-
-## Jetbrains Toolbox
-open_dmg jbt https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.11.4269.dmg
-
-## Sourcetree
-cd /tmp
-curl -L -o st.zip https://product-downloads.atlassian.com/software/sourcetree/ga/Sourcetree_3.0_200.zip
-unzip st.zip
-
